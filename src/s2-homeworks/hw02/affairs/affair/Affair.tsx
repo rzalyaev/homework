d3ecd9375bf22802d915d242a5ad0b30bcdbd6bf
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-14 01:58:10 +0400

    Add onClick to button

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -35,8 +35,7 @@ function Affair(props: AffairPropsType) {
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
-                // need to fix
-
+                onClick={deleteCallback} // need to fix
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
