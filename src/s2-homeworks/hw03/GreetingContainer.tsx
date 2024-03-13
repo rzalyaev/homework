b5c5aaffb6fe177b3c000c6e83ebf4caff91b651
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-14 02:55:19 +0400

    Add functionality to totalUsers and lastUserName

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -65,8 +65,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length // need to fix
+    const lastUserName = users[users.length - 1]?.name // need to fix
 
     return (
         <Greeting
