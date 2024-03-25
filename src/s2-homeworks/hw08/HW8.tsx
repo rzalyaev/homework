2c12f375434b3d040cc504e0d97653bacc7b3dd9
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-25 23:10:17 +0400

    Add wrapper div

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -56,42 +56,44 @@ const HW8 = () => {
     return (
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #8</div>
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.buttonsContainer}>
-                        <SuperButton
-                            id={'hw8-button-up'}
-                            onClick={sortUp}
-                            xType={currentSort === 'up' ? '' : 'secondary'}
-                        >
-                            Sort up
-                        </SuperButton>
-                        <SuperButton
-                            id={'hw8-button-down'}
-                            onClick={sortDown}
-                            xType={currentSort === 'down' ? '' : 'secondary'}
-                        >
-                            Sort down
-                        </SuperButton>
-                        <SuperButton
-                            id={'hw8-button-18'}
-                            onClick={check18}
-                            xType={currentSort === '18' ? '' : 'secondary'}
-                        >
-                            Check 18+
-                        </SuperButton>
+            <div className={s2.content}>
+                <div className={s2.hw}>
+                    <div className={s.container}>
+                        <div className={s.buttonsContainer}>
+                            <SuperButton
+                                id={'hw8-button-up'}
+                                onClick={sortUp}
+                                xType={currentSort === 'up' ? '' : 'secondary'}
+                            >
+                                Sort up
+                            </SuperButton>
+                            <SuperButton
+                                id={'hw8-button-down'}
+                                onClick={sortDown}
+                                xType={currentSort === 'down' ? '' : 'secondary'}
+                            >
+                                Sort down
+                            </SuperButton>
+                            <SuperButton
+                                id={'hw8-button-18'}
+                                onClick={check18}
+                                xType={currentSort === '18' ? '' : 'secondary'}
+                            >
+                                Check 18+
+                            </SuperButton>
+                        </div>
+
+                        <table id={'hw8-users'} className={s.users}>
+                            <thead className={s.thead}>
+                                <tr>
+                                    <td className={s.nameCol}>Name</td>
+                                    <td className={s.ageCol}>Age</td>
+                                </tr>
+                            </thead>
+
+                            <tbody>{finalPeople}</tbody>
+                        </table>
                     </div>
-
-                    <table id={'hw8-users'} className={s.users}>
-                        <thead className={s.thead}>
-                        <tr>
-                            <td className={s.nameCol}>Name</td>
-                            <td className={s.ageCol}>Age</td>
-                        </tr>
-                        </thead>
-
-                        <tbody>{finalPeople}</tbody>
-                    </table>
                 </div>
             </div>
         </div>
