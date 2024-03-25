c337f702ea2b500d3c4348d08063de22f9ac5a9f
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-25 23:12:42 +0400

    Add functionality

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,13 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            // need to fix
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+                : [...state].sort((a, b) => b.name.localeCompare(a.name));
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= 18); // need to fix
         }
         default:
             return state
